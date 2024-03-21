416ce9674e90e51263ead36b4a4d1acaf849c8a9
lunaleee <suna0902@naver.com>
2024-03-21 23:42:16 +0900

    edit contents

--- _javascript/modules/components/toc.js
+++ _javascript/modules/components/toc.js
@@ -1,11 +1,11 @@
 export function toc() {
-  if (document.querySelector('main h2, main h3')) {
+  if (document.querySelector('main h1, main h2')) {
     // see: https://github.com/tscanlin/tocbot#usage
     tocbot.init({
       tocSelector: '#toc',
       contentSelector: '.content',
       ignoreSelector: '[data-toc-skip]',
-      headingSelector: 'h2, h3, h4',
+      headingSelector: 'h1, h2, h3, h4',
       orderedList: false,
       scrollSmooth: false
     });
